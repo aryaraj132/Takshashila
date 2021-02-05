5ebbb3876dc3d139c9470e10e9e0c6dee0f6c267
Aryan Raj <61931454+aryaraj132@users.noreply.github.com>
2021-02-06 01:18:30 +0530

    Heroku tweaks

--- Takshashila/settings.py
+++ Takshashila/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
@@ -129,3 +129,5 @@ STATICFILES_DIR = [
     STATIC_DIR,
     MEDIA_ROOT,
 ]
+# Activate Django-Heroku.
+django_heroku.settings(locals())
